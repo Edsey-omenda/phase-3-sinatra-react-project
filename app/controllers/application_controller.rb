0c6240e51c719b87ae4beaecf24d2138d2f1aeaa
Edsey omenda <omendajames0317@gmail.com>
2022-09-09 03:10:32 +0300

    creates  routes  for posting  a new  model  and  deleting  a  model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,21 @@ class ApplicationController < Sinatra::Base
     models.to_json
   end
 
+  post '/models' do
+    message = Model.create(
+      title: params[:title],
+      thumbnail: params[:thumbnail],
+      short_description: params[:short_description],
+      release_date: params[:release_date],
+      city: params[:city]
+    )
+    model.to_json
+  end
+
+  delete '/models/:id' do
+    model  = Model.find(params[:id])
+    model.destroy
+    model.to_json
+  end
+
 end
